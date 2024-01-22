665d0c56ebf8152217f31096d33e62c83c9ac3be
Phat Tran <ptran@strongtie.com>
2024-01-22 07:02:53 +0700

    Finish

--- CardComparer/Program.cs
+++ CardComparer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CardComparer
 {
@@ -6,7 +7,38 @@ namespace CardComparer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter number of cards: ");
+            List<Card> cards = new List<Card>();
+
+            if (int.TryParse(Console.ReadLine(), out int noOfCards))
+            {
+                for (int i = 0; i < noOfCards; i++)
+                {
+                    cards.Add(RandomCard());
+                }
+            }    
+
+            PrintCards(cards);
+
+            Console.WriteLine("\n... sorting the cards ...\n");
+
+            cards.Sort(new CardComparerByValue());
+
+            PrintCards(cards);
+        }
+
+        static Card RandomCard()
+        {
+            Random random = new Random();
+            return new Card((Values)random.Next(1, 14), (Suits)random.Next(4));
+        }
+
+        static void PrintCards(List<Card> cards)
+        {
+            foreach (var card in cards)
+            {
+                Console.WriteLine(card.Name);
+            }
         }
     }
 }
